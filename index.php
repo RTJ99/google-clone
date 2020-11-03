69a67010c3fa2dd4a7b5d99092a4465e607edba4
RTJ99 <46841637+RTJ99@users.noreply.github.com>
2020-11-03 23:00:57 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,78 @@
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Google</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+        
+    
+    <ul>
+        <li><div id="button1"><button>Sign In</button></div></li>
+        <li><img src="grid.png" alt="grid" height="25" width="25"></li>
+        <li><a href="">Images</a></li>
+        <li><a href="">Gmail</a></li>
+        
+    </ul>
+    </head>
+    
+    <body>
+    <div id="container">
+    
+    <div style="text-align:center">
+        <img src="google-logo.png" height="95" width="260" style="padding-top:135px"  alt=" google logo ">
+    </div>
+    
+    <form method="GET" action="">
+        <input id="searchtxt" class="search" title="Search" type="text" name="results"><br>
+        <div id="buttons">
+        <input type="submit" class="button" value="Google Search">
+        <input type="submit"class="button"value="I'm Feeling Lucky">
+        <div>
+    </form>
+    <div id="buttons">
+    
+    
+    <?php
+
+        $search = $_GET['results'];
+        if(isset($_GET['results']) && $_GET['results'] != "")
+            {
+                
+                echo "<h2>Your Search Result :<h2>";
+            
+            $url = "https://www.google.com/search?q=".str_replace(' ', '%20', $_GET['results']);
+            
+            // sendRequest
+            // note how referer is set manually
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            //curl_setopt($ch, CURLOPT_REFERER, 'http://www.google.com');
+            $body = curl_exec($ch);
+            curl_close($ch);
+            echo $body;
+            // now, process the JSON string
+            //$json = json_decode($body);
+            // print out the Array
+            
+            //print_r($json);
+            
+            // now have some fun with the results...
+        }
+
+?>
+    <div id="buttons">
+    
+    </script>
+    
+    </div>
+    
+    
+    </div>
+    
+    
+    </body>
+    
+    
+    </html>
